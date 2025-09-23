1b6a66bc793547ac47d0c1192e4474a75e48b569
sikara <987555458@qq.com>
2025-09-23 16:22:51 +0800

    feat(frpc): 添加基础配置表单组件和中文翻译
    添加 FrpcConfDrawer 组件用于修改基础配置
    实现 FrpcCommonConfigForm 表单组件
    添加 frpc 配置的中文翻译
    扩展 ClientCommonConfig 类型定义

---
+++ src/types/frpc-common-config-zh.ts
@@ -0,0 +1,14 @@
+export const frpcCommonConfigZh: Record<string, string> = {
+  user: "用户名",
+  serverAddr: "连接服务端的地址",
+  serverPort: "连接服务端的端口，默认为 7000",
+  natHoleStunServer: "xtcp 打洞所需的 stun 服务器地址",
+  dnsServer: "DNS 服务器地址",
+  loginFailExit: "第一次登陆失败后是否退出",
+  udpPacketSize: "代理 UDP 服务时支持的最大包长度，默认为 1500",
+  auth: "客户端鉴权配置",
+  webServer: "客户端 AdminServer 配置",
+  transport: "客户端网络层配置",
+  log: "日志配置",
+  virtualNet: "虚拟网络配置",
+};
