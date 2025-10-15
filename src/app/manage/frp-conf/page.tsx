99592eac62298f2cca0b78e243883cd14a6decc0
sikara <987555458@qq.com>
2025-10-15 10:46:45 +0800

    fix(frp-conf): 修正默认配置值为空字符串而非空对象

--- src/app/manage/frp-conf/page.tsx
+++ src/app/manage/frp-conf/page.tsx
@@ -2,7 +2,7 @@ import FrpEditor from "@/components/client/frp-editor";
 import { getConf } from "@/lib/server-action";
 
 export default async function FrpConfPage() {
-  const data = (await getConf(true)) || {};
+  const data = (await getConf(true)) || "";
 
   return (
     <div className="flex flex-col items-center h-full">
